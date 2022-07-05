a798f493963a5a3ae7136d3c51e34c9f7ba9cde2
Stephen Wills <stephenrwills94@gmail.com>
2022-07-05 10:52:20 -0500

    finished main method

---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine("What is your pet's name?");
+var pet = Console.ReadLine();
+
+Console.WriteLine("What is your favorite park?");
+var park = Console.ReadLine();
+
+Console.WriteLine("What is your favorite day of the week?");
+var dayOfTheWeek = Console.ReadLine();
+
+Console.WriteLine("What is your favorite band?");
+var favoriteBand = Console.ReadLine();
+
+Console.WriteLine($"Hi my name is {name}, on {dayOfTheWeek} I can be found in {park} walking {pet}, listening to {favoriteBand}");
+
+
+
